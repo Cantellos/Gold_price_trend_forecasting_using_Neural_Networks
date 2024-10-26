ee9e74098d9ac6848ae492594eea1df093546b0d
Cantellos <andrea.cantelli@edu.unife.it>
2024-10-26 18:46:48 +0200

    updating cleaning data file

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,10 +1,21 @@
-#open the dataset C:/Users/andre/Documents/GitHub/Lab-DL/dataset/XAU_1w_data_2004_to_2024-09-20.csv and clean it
 import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
+from pathlib import Path
 
-path = os.path.join('..', 'dataset', 'XAU_1w_data_2004_to_2024-09-20.csv')
-df = pd.read_csv(path)
+#TODO: import pandas
+
+# Define the base directory (project root)
+base_dir = Path(__file__).resolve().parent.parent  # Goes up two levels from src/ to space/
+
+# Define the relative path to the data file containing the corpus of the files to be analyzed
+data_file_path = base_dir / 'data' / 'XAU_1w_data_2004_to_2024-09-20.csv'
+
+# Convert the path to a string with forward slashes
+input_directory_path = data_file_path.as_posix()
+
+# Load the data from the CSV file
+df = pd.read_csv(input_directory_path)
+
+# Conta i valori mancanti per ogni colonna
+missing_values = df.isnull().sum()
+# Visualizza le righe con valori mancanti
+missing_data = df[df.isnull().any(axis=1)]
--- dataset/fixingdata.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# Conta i valori mancanti per ogni colonna
-missing_values = df.isnull().sum()
-# Visualizza le righe con valori mancanti
-missing_data = df[df.isnull().any(axis=1)]
