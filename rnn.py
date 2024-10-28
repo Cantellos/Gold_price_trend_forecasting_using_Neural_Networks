909aaa0f212ce9a8865a5a52f44b72d7d1787b0c
Cantellos <andrea.cantelli@edu.unife.it>
2024-10-28 16:03:18 +0100

    packages for rnn installed and made the set up for RNN or GRU or LSTM

--- rnn.py
+++ rnn.py
@@ -1,28 +1,33 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import SimpleRNN, Dense
+import matplotlib.pyplot as plt
+import pandas as pd
+from keras.api.models import Sequential
+from keras.api.layers import Dense, LSTM,GRU,SimpleRNN
+from sklearn.preprocessing import MinMaxScaler
 
-from data_cleaning import load_and_clean_data  # Assuming this function is defined in data_cleaning.py
+df = pd.read_csv('airline-passengers.csv')
+df.head()
+df.drop(['Month'],axis=1,inplace=True)
+dataset = np.array(df)
+dataset.reshape(-1,1)
 
-# Load and clean the dataset
-X_train, y_train, X_test, y_test = load_and_clean_data()
+plt.plot(dataset)
 
-# Define the RNN model
-model = Sequential()
-model.add(SimpleRNN(50, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=False))
-model.add(Dense(1, activation='sigmoid'))
+scaler = MinMaxScaler()
+dataset = scaler.fit_transform(dataset)
 
-# Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
-
-# Evaluate the model
-loss, accuracy = model.evaluate(X_test, y_test)
-print(f'Test Accuracy: {accuracy}')
-
-# train set: 0.8 
-# validation set: 0.1
-# test set: 0.1
+train_size = int(len(dataset) * 0.75)
+test_size = len(dataset) - train_size
+train=dataset[:train_size,:]
+test=dataset[train_size:142,:]
+def getdata(data,lookback):
+    X,Y=[],[]
+    for i in range(len(data)-lookback-1):
+        X.append(data[i:i+lookback,0])
+        Y.append(data[i+lookback,0])
+    return np.array(X),np.array(Y).reshape(-1,1)
+lookback=1
+X_train,y_train=getdata(train,lookback)
+X_test,y_test=getdata(test,lookback)
+X_train=X_train.reshape(X_train.shape[0],X_train.shape[1],1)
+X_test=X_test.reshape(X_test.shape[0],X_test.shape[1],1)
